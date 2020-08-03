31c4228ddeb4d1a65b01f0bf51607868d8195203
Dean Gelbaum <deanscottg@gmail.com>
2020-08-03 22:10:16 +0000

    Automatically backed up by Learn

---
+++ bug.rb
@@ -0,0 +1,5 @@
+def multiply (num1, num2)
+  puts "multiply got called with #{num1} and  #{num2}"
+  num1 * num2 
+end
+multiply (2, 4)
---
+++ new.rb
@@ -0,0 +1,16 @@
+# def a_method (a, b)
+#   puts "I got #{a}"
+#   puts "I got #{b}"
+#   sum = a + b 
+#   puts "I got #{sum}"
+#   sum
+  
+# end
+# a_method(2,3)
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+    return "Martha Stewart"
+    "Guy Fieri"
+  
+end
